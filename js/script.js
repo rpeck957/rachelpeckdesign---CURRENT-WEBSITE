d40294663fd89a370f58fcd5f69523245e96fe38
rpeck957 <rachelpeck@Rachels-MBP.fios-router.home>
2017-12-15 00:15:51 -0500

    12.14.2017
    Project page & work gallery

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    // console.log( "ready!" );
+    $('.hamburger').click(function(){
+      $('.mobile').toggleClass('visible');
+    });
+});
+
+
+// toggle between the hide() and show()
+
+$("button").click(function(){
+    $(".print").toggle();
+});
+
+
+
+// SLIDESHOW
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("projectimage");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
