954d05acfa6d4e38d748abd71012c01dacd079fd
rpeck957 <rachelpeck@Rachels-MBP.fios-router.home>
2017-12-30 02:16:09 -0500

    12.30.17
    Post meeting, changes back with new changes hidden in css. Filter on
    gallery. Potentially use grid (hidden in css) for cards instead of flex

--- js/script.js
+++ js/script.js
@@ -6,35 +6,14 @@ $( document ).ready(function() {
 });
 
 
-
-// SLIDESHOW
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("projectimage");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// gallery filter
+var $btns = $('.btn').click(function() {
+  if (this.id == 'all') {
+    $('#parent > a').fadeIn(450);
+  } else {
+    var $el = $('.' + this.id).fadeIn(450);
+    $('#parent > a').not($el).hide();
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  $btns.removeClass('active');
+  $(this).addClass('active');
+})
