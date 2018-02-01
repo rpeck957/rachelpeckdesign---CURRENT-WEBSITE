1f74aafbcd62474531e7e1ac71aca4e1bd2e3cc4
rpeck957 <rachelpeck@Rachels-MBP.fios-router.home>
2018-01-31 21:18:56 -0500

    1.31.18
    Sliding design. Adding back to top button, dribble link, fixing some h
    tag issues, adding in copy to photography page and index, new index
    design, change index background cover to fixed*** to fix scrolling and
    stretching issue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MOBILE HAMBURGER
 $( document ).ready(function() {
     // console.log( "ready!" );
     $('.hamburger').click(function(){
@@ -15,7 +16,7 @@ $( document ).ready(function() {
 // });
 
 
-// gallery filter
+// GALLERY FILTER
 var $btns = $('.btn').click(function() {
   if (this.id == 'all') {
     $('#parent > a').fadeIn(450);
@@ -26,3 +27,22 @@ var $btns = $('.btn').click(function() {
   $btns.removeClass('active');
   $(this).addClass('active');
 })
+
+// BACK TO TOP BUTTON
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
